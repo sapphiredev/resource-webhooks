b2e1814d7abb4019a326e037cc6e7554cf0f289f
Jeroen Claassens <support@favware.tech>
2023-06-29 19:42:55 +0200

    fix: use daisyui 3 loading indicator

--- components/loading-indicator.vue
+++ components/loading-indicator.vue
@@ -1,12 +1,7 @@
 <template>
 	<div v-if="isLoadingIndicator" role="presentation" class="fixed z-[1200] block h-full w-full bg-base-300 opacity-75">
 		<div class="absolute left-1/2 top-1/2 z-[1200]">
-			<div
-				class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-				role="status"
-			>
-				<span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
-			</div>
+			<div class="loading loading-bars loading-lg"></div>
 		</div>
 	</div>
 </template>
