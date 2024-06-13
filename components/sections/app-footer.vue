5310bbee4514f49593b26655d5f5e3d6089cd464
Jeroen Claassens <support@favware.tech>
2024-06-13 23:10:48 +0200

    docs: remove vercel sponsorship

--- components/sections/app-footer.vue
+++ components/sections/app-footer.vue
@@ -5,11 +5,6 @@
 				<p class="mb-2 mt-4 max-w-full flex-grow-0 basis-full text-center text-sm opacity-70">
 					Copyright © 2022 The Sapphire Community and its contributors
 				</p>
-
-				<sponsor-link href="https://vercel.com/?utm_source=sapphiredev&utm_campaign=oss" title="Vercel" class="mr-2">
-					<img alt="Logo Vercel" src="/icons/powered-by-vercel.svg" style="height: 40px" />
-				</sponsor-link>
-
 				<sponsor-link href="https://polypane.app/" title="Polypane">
 					<img alt="Logo Polypane" src="/icons/powered-by-polypane.svg" style="height: 40px" />
 				</sponsor-link>
