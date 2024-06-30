052ef5a15a6159d19bb73ce328702265fad8b583
Jeroen Claassens <support@favware.tech>
2024-06-30 14:23:12 +0200

    fix: change theme toggle back to a button for better UI

--- components/theme-button.vue
+++ components/theme-button.vue
@@ -1,20 +1,16 @@
 <template>
-	<label class="flex cursor-pointer gap-2">
-		<MoonIcon class="h-5 w-5" />
-		<input
-			aria-label="Theme Switcher toggle"
-			type="checkbox"
-			:value="currentColor"
-			class="theme-controller toggle"
-			@change="currentColor = currentColor === 'dark' ? 'light' : 'dark'"
-		/>
-		<SunIcon class="h-5 w-5" />
-	</label>
+	<button
+		aria-label="Theme Switcher Button"
+		class="btn btn-ghost content-center"
+		@click="currentColor = currentColor === 'dark' ? 'light' : 'dark'"
+	>
+		<MoonIcon v-if="currentColor === 'dark'" class="h-6 w-6" />
+		<SunIcon v-else class="h-6 w-6" />
+	</button>
 </template>
 
 <script setup lang="ts">
 import { MoonIcon, SunIcon } from '@heroicons/vue/24/solid';
-
 const currentColor = useColorMode({
 	selector: 'html',
 	attribute: 'data-theme'
