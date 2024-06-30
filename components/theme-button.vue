845fc8ad204725a6edf8646791b236c20f265095
Jeroen Claassens <support@favware.tech>
2024-06-30 13:51:19 +0200

    fix: fixed theme button

--- components/theme-button.vue
+++ components/theme-button.vue
@@ -1,24 +1,22 @@
 <template>
-	<!-- <button
-		aria-label="Theme Switcher Button"
-		class="btn btn-ghost content-center"
-		@click="currentColor = currentColor === 'dark' ? 'light' : 'dark'"
-	>
-		<moon-icon v-if="currentColor === 'dark'" />
-		<sun-icon v-else />
-	</button> -->
-
 	<label class="flex cursor-pointer gap-2">
-		<MoonIcon />
-		<input aria-label="Theme Switcher toggle" type="checkbox" value="light" class="theme-controller toggle" />
-		<SunIcon />
+		<MoonIcon class="h-5 w-5" />
+		<input
+			aria-label="Theme Switcher toggle"
+			type="checkbox"
+			:value="currentColor"
+			class="theme-controller toggle"
+			@change="currentColor = currentColor === 'dark' ? 'light' : 'dark'"
+		/>
+		<SunIcon class="h-5 w-5" />
 	</label>
 </template>
 
 <script setup lang="ts">
 import { MoonIcon, SunIcon } from '@heroicons/vue/24/solid';
-// const currentColor = useColorMode({
-// 	selector: 'html',
-// 	attribute: 'data-theme'
-// });
+
+const currentColor = useColorMode({
+	selector: 'html',
+	attribute: 'data-theme'
+});
 </script>
