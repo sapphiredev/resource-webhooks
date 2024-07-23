d7bdc0f3741c7190c2abfd16ad932d751da8fd14
Jeroen Claassens <support@favware.tech>
2024-07-23 21:06:06 +0200

    refactor: use an alert for the form error

--- components/forms/error-message.vue
+++ components/forms/error-message.vue
@@ -1,7 +1,15 @@
 <template>
-	<VeeErrorMessage :name="name" class="label-text-alt" :class="{ 'text-red-500': !!hasErrorMessage }" />
+	<div v-show="!!hasErrorMessage" role="alert" class="alert alert-error">
+		<XCircleIcon class="h-7 w-7 text-white" />
+		<div>
+			<VeeErrorMessage :name="name" class="text-white" />
+			<div class="text-xs italic text-white">Resolve this error to submit</div>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
+import { XCircleIcon } from '@heroicons/vue/24/outline';
+
 defineProps<{ name: string; hasErrorMessage: boolean }>();
 </script>
